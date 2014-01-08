dd926832a314367244d3efe9bd048df8722bf086
brianthecoder <brian@20jeans.com>
2014-01-08 02:14:19 -0800

    use between instead

--- app/models/spree/shipment_decorator.rb
+++ app/models/spree/shipment_decorator.rb
@@ -2,10 +2,11 @@ Spree::Shipment.class_eval do
   scope :exportable, -> { joins(:order).where('spree_shipments.state != ?', 'pending') }
 
   def self.between(from, to)
-    joins(:order).where('(spree_shipments.updated_at > ? AND spree_shipments.updated_at < ?) OR (spree_orders.updated_at > ? AND spree_orders.updated_at < ?)',from, to, from, to)
+    joins(:order).where('(spree_shipments.updated_at BETWEEN ? AND ?) OR (spree_orders.updated_at BETWEEN ? AND ?)',from.beginning_of_day, to, from.beginning_of_day, to)
   end
 
-private
+  private
+
   def send_shipped_email
     Spree::ShipmentMailer.shipped_email(self).deliver if Spree::Config.send_shipped_email
   end
