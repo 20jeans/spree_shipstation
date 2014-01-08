ed6f8d6d411ef2f366d9fc7289596860bbe11d43
brianthecoder <brian@20jeans.com>
2014-01-08 02:14:54 -0800

    formatting and cleanup

--- lib/spree/basic_ssl_authentication.rb
+++ lib/spree/basic_ssl_authentication.rb
@@ -7,7 +7,8 @@ module Spree
       before_filter :authenticate
     end
 
-  protected
+    protected
+
     def authenticate
       authenticate_or_request_with_http_basic do |username, password|
         username == Spree::Config.shipstation_username && password == Spree::Config.shipstation_password
--- lib/spree/shipment_notice.rb
+++ lib/spree/shipment_notice.rb
@@ -2,24 +2,21 @@ module Spree
   class ShipmentNotice
     attr_reader :error
 
-    def initialize(params) 
+    def initialize(params)
       @number   = params[:order_number]
       @tracking = params[:tracking_number]
     end
 
     def apply
       locate ? update : not_found
-    rescue => e
-      handle_error(e)
     end
 
   private
     def locate
-      if Spree::Config.shipstation_number == :order
-        order = Spree::Order.find_by_number(@number)
-        @shipment = order.try(:shipment)
+      @shipment = if Spree::Config.shipstation_number == :order
+        Spree::Order.where(number: @number).first.try(:shipment)
       else
-        @shipment = Spree::Shipment.find_by_number(@number)
+        Spree::Shipment.where(number: @number).first
       end
     end
 
@@ -28,8 +25,8 @@ module Spree
 
       unless @shipment.shipped?
         @shipment.reload.update_attribute(:state, 'shipped')
-        @shipment.inventory_units.each &:ship!
-        @shipment.touch :shipped_at
+        @shipment.inventory_units.each(&:ship!)
+        @shipment.touch(:shipped_at)
       end
 
       true
@@ -39,7 +36,7 @@ module Spree
       @error = I18n.t(:shipment_not_found, number: @number)
       false
     end
-    
+
     def handle_error(error)
       Rails.logger.error(error)
       @error = I18n.t(:import_tracking_error, error: error.to_s)
